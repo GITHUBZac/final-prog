25415f9571c290bfef97616012c5078389f3ec9d
Segundobenito <sbenito@udesa.edu.ar>
2023-05-26 18:10:23 -0300

    arrancando

---
+++ src/screens/Home.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+
+ class Home extends Component {
+     constructor(props) {
+         super(props)
+     }
+
+     ejecutarConAccionCorta() {
+         console.log('El Cliente nos pidio algo');
+     }
+
+     ejecutarConAccionLarga() {
+         console.log('El cliente nos pidio algo Largo');
+     }
+
+     
+    render() {
+        return (
+            <View>
+                <Text>  HOME </Text>
+
+                <TouchableOpacity 
+                style = {styles.button}
+                onPress = {()=>this.ejecutarConAccionCorta()}
+                onLongPress = {() => this.ejecutarConAccionLarga()}
+                >
+                    <Text style = {styles.textoBtn}>
+                        Algun dia sere Boton
+                    </Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    button:{
+        backgroundColor: 'green',
+        borderRadius: 20,
+        borderWidth: 2,
+        textAlign: 'center',
+        padding: 10,
+        
+    },
+    textoBtn: {
+        color:"white",
+
+    }
+})
+export default Home
---
+++ src/screens/Register.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+
+ class Register extends Component {
+     constructor(props){
+         super(props)
+        
+     }
+
+    render() {
+        return (
+            <View>
+                <Text> Aqui vamos a tener nuestro registro </Text>
+            
+            </View>
+        )
+    }
+}
+export default Register
