965d1de601de2e7fbe22384d7f69c6d9175f36a6
tomasarizu <tarizu@udesa.edu.ar>
2023-05-31 15:09:24 -0300

    screen login casi terminada

---
+++ src/screens/Login.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import { Text, View, TextInput, TouchableOpacity, StyleSheet, TouchableHighlightBase } from 'react-native'
+import {auth, db} from '../firebase/config';
+
+
+ class Login extends Component {
+     constructor(props) {
+         super(props)
+         this.state = {
+            inputMail : "",
+            inputPassword : "",
+            error: ""
+            
+         }
+
+     }
+
+
+     iniciarUsuarioLogin(email,pass){
+         auth.signInWithEmailAndPassword(email,pass)
+         .then(res=>{
+             this.setState({
+                 error: "",
+             })
+             this.props.navigation.navigate("HomeNav")
+         })
+     }
+
+
+    render() {
+        return (
+
+            <View>
+
+        <Text onPress={() => this.props.navigation.navigate('Register') }>Volver al Register</Text>
+
+                <Text>
+                    {this.state.error}
+                </Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder = 'digita tu correo electronico'
+                    keyboardType = "email-address"
+                    onChangeText = {(text) => this.setState({inputMail: text})}
+                    value = {this.state.inputMail}
+                />
+
+                <TextInput 
+                    style={styles.input}
+                    placeholder = 'digita tu password'
+                    onChangeText = {(text) => this.setState({inputPassword: text})}
+                    value = {this.state.inputPassword}
+                    secureTextEntry = {true}
+                />
+               
+
+                
+                {
+                    this.state.inputMail=="" || this.state.inputPassword == ""  ?
+                    <TouchableOpacity style = {styles.btnerror}>
+                    <Text style = {styles.buttonText}> Loguearme</Text>
+                  </TouchableOpacity>
+                :
+                <TouchableOpacity style = {styles.btn}onPress = {() => this.iniciarUsuarioLogin(this.state.inputMail, this.state.inputPassword)}>
+                    <Text style = {styles.btnText}> Loguearme</Text>
+                
+               </TouchableOpacity>
+                }
+
+            </View>
+
+                
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        borderColor: '#3d3d3d',
+        marginTop: 24,
+        height: 24,
+        padding: 5
+    },
+    btn: {
+        backgroundColor: '#54d0e0',
+        padding: 10,
+        borderRadius: 20,
+        marginTop: 32
+    },
+    btnText : {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'green'
+    },
+    buttonText : {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'grey'
+    }
+})
+
+export default Login
