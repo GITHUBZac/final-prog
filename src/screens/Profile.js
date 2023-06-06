a4dbf5e56089bc66ac4b61351ef8475d4338363e
tomasarizu <tarizu@udesa.edu.ar>
2023-06-06 19:06:48 -0300

    profile, profiledata

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import ProfileData from '../components/Profile/ProfileData'
 
  class Profile extends Component {
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <Text> Perfil </Text>
+                <ProfileData navigation = {this.props.navigation}/>
+
             </View>
         )
     }
