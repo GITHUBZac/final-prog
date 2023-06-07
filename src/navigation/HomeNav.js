44842c48a97546ffe4d4dffcc51efab05d69d9b7
tomasarizu <tarizu@udesa.edu.ar>
2023-06-07 17:00:51 -0300

    arrancando busqueda

--- src/navigation/HomeNav.js
+++ src/navigation/HomeNav.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
+import {FontAwesome} from '@expo/vector-icons'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {AntDesign} from '@expo/vector-icons';
 
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
-import NewPost from '../screens/NewPost'
+import NewPost from '../screens/NewPost';
+import Buscador from '../screens/Buscador';
 
 
 const Tab = createBottomTabNavigator();
@@ -15,6 +16,14 @@ export default class HomeNav extends Component {
     render() {
         return (
             <Tab.Navigator >
+                <Tab.Screen
+        name= 'Search'
+        component={Buscador}
+        options = {{  headerShown: false ,
+          tabBarIcon: () => <FontAwesome name='search' size={24} color='grey'/>
+          
+          }}
+      />
                 <Tab.Screen name = 'Home' component = {Home} 
                 options = {{tabBarIcon: () => <AntDesign name = 'home' color = 'black' size = {24} /> }} />
 
---
+++ src/screens/Buscador.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { db, auth } from "../firebase/config"
+
+class Buscador extends Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            infoUser: [],
+            allUsers: [],
+            busqueda:''
+        };
+    }
+    componentDidMount() {
+        db.collection('users').onSnapshot(docs => {
+            let users = []
+            docs.forEach(doc => {
+                users.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            this.setState({
+                infoUser: [],
+                allUsers: users
+            },
+                () => console.log(this.state.infoUser)
+            )
+        })
+    }
+    // evitarSubmit(event) {
+    //     event.preventDefault();
+    // }
+
+    buscador(usuarioBuscado) {
+        let resultadoBusqueda = []
+        usuarioBuscado == '' ?
+            this.setState({ infoUser: resultadoBusqueda, busqueda: usuarioBuscado }) :
+            resultadoBusqueda = this.state.allUsers.filter((item) => {
+                return (item.data.creador.includes(usuarioBuscado) ? item.data.creador.includes(usuarioBuscado) : item.data.nombreDeUsuario.includes(usuarioBuscado))
+            })
+        this.setState({ infoUser: resultadoBusqueda, busqueda: usuarioBuscado })
+
+    }
+
+    render() {
+        return (
+            <View style={styles.contenedor}>
+                <TextInput
+                    style={styles.input}
+                    keyboardType='default'
+                    placeholder='Busca aca!'
+                    onChangeText={(text) => this.buscador(text)}
+                    value={this.state.busqueda}
+                />
+                {/* <TouchableOpacity onSubmit={(event) => this.evitarSubmit(event)} style={styles.to}>
+                    <Text>Search</Text>
+                </TouchableOpacity>  */}
+                <Text>Quizas conozcas...</Text>
+                {(this.state.infoUser.length == 0 && this.state.busqueda != '') ?
+                    <Text>El email o el nombre de usuario no existe</Text>
+                    :
+                    <FlatList
+                        data={this.state.infoUser}
+                        keyExtractor={item => item.id.toString()}
+                        renderItem={({ item }) => <TouchableOpacity onPress={() => auth.currentUser.email === item.data.creador ? this.props.navigation.navigate('Profile', { email: item.data.creador }) : this.props.navigation.navigate('ProfileAmigo', { email: item.data.creador })}>
+                            <Text> {item.data.creador} ({item.data.nombreDeUsuario})</Text>
+                        </TouchableOpacity>} //RENDERIZA UN COMPONENTE POST que le paso a traves de la prop data toda la info que se guarda en items (data sale del push de doc.data
+                    />                     
+                }
+            </View>
+
+        );
+    }
+}
+const styles = StyleSheet.create({
+    contenedor:{
+        backgroundColor: 'rgb(128, 128, 128)',
+        flex: 1
+    },
+   
+    input: {
+        fontFamily: 'monospace',
+        fontSize: 20,
+        borderWidth: 2,
+        height: 50,
+        width: '90%',
+        borderRadius: 40,
+        borderColor: 'rgb (0,0,0)',
+        padding: 15,
+        margin: 15,
+        backgroundColor: 'rgb(128, 128, 128)',
+        color: 'rgb(0,0,0)'
+    },
+    to: {
+        width: 200,
+        height: 50,
+        margin: 5,
+        backgroundColor: 'deepskyblue',
+        textAlign: 'center',
+        borderRadius: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10
+    }
+})
+
+export default Buscador
