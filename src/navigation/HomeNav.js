b20c14eebef77f3a5450d4697d3bae54b8829fbe
GITHUBZac <zofarrell@udesa.edu.ar>
2023-06-11 10:50:32 -0300

    ordenando

--- src/navigation/HomeNav.js
+++ src/navigation/HomeNav.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import {FontAwesome} from '@expo/vector-icons'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import {AntDesign} from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { AntDesign } from '@expo/vector-icons';
 
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
@@ -16,28 +16,31 @@ export default class HomeNav extends Component {
     render() {
         return (
             <Tab.Navigator >
+                <Tab.Screen name='Home' component={Home}
+                    options={{ tabBarIcon: () => <AntDesign name='home' color='black' size={24} /> }} />
+
                 <Tab.Screen
-        name= 'Search'
-        component={Buscador}
-        options = {{  headerShown: false ,
-          tabBarIcon: () => <FontAwesome name='search' size={24} color='grey'/>
-          
-          }}
-      />
-                <Tab.Screen name = 'Home' component = {Home} 
-                options = {{tabBarIcon: () => <AntDesign name = 'home' color = 'black' size = {24} /> }} />
+                    name='Search'
+                    component={Buscador}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => <FontAwesome name='search' size={24} color='grey' />
 
-                <Tab.Screen name = 'Profile' component = {Profile} 
-                options = {{tabBarIcon: () => <AntDesign name = "profile" color = "black" size = {24} /> }} />
+                    }}
+                />
 
-            <Tab.Screen name = 'NewPost' component = {NewPost} 
-                options = {{tabBarIcon: () => <AntDesign name = "plus" color = "black" size = {24} /> }} />
- 
-        
-             </Tab.Navigator>
-        
+                <Tab.Screen name='NewPost' component={NewPost}
+                    options={{ tabBarIcon: () => <AntDesign name="plus" color="black" size={24} /> }} />
 
-            
+                <Tab.Screen name='Profile' component={Profile}
+                    options={{ tabBarIcon: () => <AntDesign name="profile" color="black" size={24} /> }} />
+
+
+
+            </Tab.Navigator>
+
+
+
         )
     }
 }
