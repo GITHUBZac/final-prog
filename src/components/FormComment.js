38864c06952417fceae7dffc1269a43c6233f8d3
GITHUBZac <zofarrell@udesa.edu.ar>
2023-06-12 11:47:55 -0300

    comentarios hecho

---
+++ src/components/FormComment.js
@@ -0,0 +1,49 @@
+import { Text, View, TextInput, TouchableOpacity, StyleSheet} from 'react-native'
+import React, { Component } from 'react'
+import { db, auth } from '../firebase/config'
+import firebase from 'firebase'
+
+export default class FormComment extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            comentario: ''
+        }
+    }
+
+    crearComentario(comentario){
+        db.collection('posts')
+        .doc(this.props.idPost)
+        .update({
+            comentarios: firebase.firestore.FieldValue.arrayUnion({
+                owner: auth.currentUser.email,
+                createdAt: Date.now(),
+                comentario: comentario
+            })
+        })
+    }
+
+    render() {
+    return (
+      <View>
+        <TextInput 
+            keyboardType='default'
+            style = {styles.input}    
+            onChangeText = {text => this.setState({comentario: text})}
+            value = {this.state.comentario}
+            placeholder = 'Agrega tu comentario aquí'
+        />
+        <TouchableOpacity onPress={()=> this.crearComentario(this.state.comentario)}>
+            <Text>Apreta para comentar</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    input:{
+      borderColor : 'red',
+      borderWidth : 2
+    }
+  })
--- src/screens/Comments.js
+++ src/screens/Comments.js
@@ -1,17 +1,35 @@
-import { Text, View } from 'react-native'
+import { Text, View, FlatList} from 'react-native'
 import React, { Component } from 'react'
+import FormComment from '../components/FormComment';
+import { db, auth } from '../firebase/config';
 
 export default class Comments extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            data: {}
+        }
+    }
 
+    componentDidMount(){
+        db.collection('posts').doc(this.props.route.params.id).onSnapshot( doc => {
+            this.setState({
+                data: doc.data()
+            }),()=> console.log(this.state.data.comentarios)
+        })
     }
   
     render() {
     console.log(this.props);
     return (
       <View>
-        <Text>Comments</Text>
+        <Text>Aca podras ver todos los comentarios del posteo</Text>
+        <FlatList
+            data={this.state.data.comentarios}
+            keyExtractor={item => item.createdAt.toString()}
+            renderItem={({item}) => <Text>{item.comentario}</Text>}
+        />
+        <FormComment idPost={this.props.route.params.id}/>
       </View>
     )
   }
